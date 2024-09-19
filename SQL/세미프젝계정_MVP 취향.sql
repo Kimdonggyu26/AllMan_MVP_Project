95fe2236d918d5c7776006ea21addba692982c46
asdnfldi1 <asdnfldi1@naver.com>
2024-09-19 18:03:59 +0900

    DB 생성

---
+++ SQL/세미프젝계정_MVP 취향.sql
@@ -0,0 +1,18 @@
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(10, 'NDHE', 'RED', '서사 중심의 역사 드라마 애호가', '깊이 있는 이야기 + 역사적 배경 + 무거운 주제 + 현실 기반');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(20, 'VDSE', 'ORANGE', '비주얼 중심의 SF 마니아', '시각적 아름다움 + 미래적 배경 + 유머 있는 톤 + 판타지적 세계관');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(30, 'SPTF', 'YELLO', '진지한 심리 스릴러 팬', '이야기의 깊이 + 현실 기반 + 무거운 주제 + 내적 갈등');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(40, 'BOFA', 'GREEN', '밝고 긍정적인 판타지 모험가', '시각적 아름다움 + 판타지적 배경 + 유머 있는 톤 + 영웅적 인물');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(50, 'ETRF', 'SKY', '감성적인 로맨스 팬', '이야기의 깊이 + 현실적 인물 + 가벼운 톤 + 명확한 결말');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(60, 'CCFE', 'BLUE', '창의적이고 도전적인 영화 선호자', '이야기의 깊이 + 미래적 배경 + 복잡한 주제 + 열린 결말');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(70, 'CAES', 'PURPLE', '코미디와 오락성을 중시하는 관객', '시각적 아름다움 + 유머 있는 톤 + 일상적 인물 + 명확한 결말');
+INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
+VALUES(80, 'HHAL', 'PINK', '역사 서사적 액션 마니아', '시각적 아름다움 + 역사적 배경 + 영웅적 인물 + 명확한 결말');
+
+COMMIT;
