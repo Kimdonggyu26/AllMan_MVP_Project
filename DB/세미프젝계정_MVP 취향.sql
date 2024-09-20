86e1b19a0dd94303a3ac3b71fdbaa81f650cb23a
Junsu001 <anima9125@gmail.com>
2024-09-20 12:42:49 +0900

    9/20 관리자메인페이지(css포함) 완성
    author : 김동규
    관리자메인페이지.html
    관리자메인페이지.css
    완성

--- DB/세미프젝계정_MVP 취향.sql
+++ DB/세미프젝계정_MVP 취향.sql
@@ -1,18 +1,16 @@
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(10, 'NDHE', 'RED', '서사 중심의 역사 드라마 애호가', '깊이 있는 이야기 + 역사적 배경 + 무거운 주제 + 현실 기반');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(20, 'VDSE', 'ORANGE', '비주얼 중심의 SF 마니아', '시각적 아름다움 + 미래적 배경 + 유머 있는 톤 + 판타지적 세계관');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(30, 'SPTF', 'YELLO', '진지한 심리 스릴러 팬', '이야기의 깊이 + 현실 기반 + 무거운 주제 + 내적 갈등');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(40, 'BOFA', 'GREEN', '밝고 긍정적인 판타지 모험가', '시각적 아름다움 + 판타지적 배경 + 유머 있는 톤 + 영웅적 인물');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(50, 'ETRF', 'SKY', '감성적인 로맨스 팬', '이야기의 깊이 + 현실적 인물 + 가벼운 톤 + 명확한 결말');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(60, 'CCFE', 'BLUE', '창의적이고 도전적인 영화 선호자', '이야기의 깊이 + 미래적 배경 + 복잡한 주제 + 열린 결말');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(70, 'CAES', 'PURPLE', '코미디와 오락성을 중시하는 관객', '시각적 아름다움 + 유머 있는 톤 + 일상적 인물 + 명확한 결말');
-INSERT INTO TASTE(TASTE_NO, TASTE_CODE, TASTE_COLOR, TASTE_INFO, TASTE_PT) 
-VALUES(80, 'HHAL', 'PINK', '역사 서사적 액션 마니아', '시각적 아름다움 + 역사적 배경 + 영웅적 인물 + 명확한 결말');
+INSERT INTO MOVIE (MOVIE_NO, MOVIE_TITLE, MOVIE_CONTENT, GENRE, PLAYTIME, COUNTRY, AGE_LV, OPEN_DATE, DIRECTOR, AUDIENCE_COUNT, ACTOR, PREVIEW, STATUS, GRADE, MAIN_PATH, TASTE_NO) 
+VALUES (SEQ_MOVIENO.NEXTVAL, '인셉션', '꿈 속에서 꿈을 꾸며 범죄를 실행하는 이야기.', 'SF, 스릴러', 148, '미국', '12세 이상', '2010-07-21', '크리스토퍼 놀란', 800, '레오나르도 디카프리오, 조셉 고든 레빗', 'https://youtube.com/inception_trailer', '10', 9.0, '/images/inception.jpg', 10);
 
-COMMIT;
+INSERT INTO MOVIE (MOVIE_NO, MOVIE_TITLE, MOVIE_CONTENT, GENRE, PLAYTIME, COUNTRY, AGE_LV, OPEN_DATE, DIRECTOR, AUDIENCE_COUNT, ACTOR, PREVIEW, STATUS, GRADE, MAIN_PATH, TASTE_NO) 
+VALUES (SEQ_MOVIENO.NEXTVAL, '기생충', '부자와 가난한 가족 사이의 미묘한 관계를 그린 영화.', '드라마, 스릴러', 132, '한국', '15세 이상', '2019-05-30', '봉준호', 1000, '송강호, 이선균, 조여정', 'https://youtube.com/parasite_trailer', '10', 9.5, '/images/parasite.jpg', 20);
+
+INSERT INTO MOVIE (MOVIE_NO, MOVIE_TITLE, MOVIE_CONTENT, GENRE, PLAYTIME, COUNTRY, AGE_LV, OPEN_DATE, DIRECTOR, AUDIENCE_COUNT, ACTOR, PREVIEW, STATUS, GRADE, MAIN_PATH, TASTE_NO) 
+VALUES (SEQ_MOVIENO.NEXTVAL, '어벤져스: 엔드게임', '어벤져스 팀이 타노스에 맞서 우주를 구하기 위한 최후의 전투를 벌인다.', '액션, 모험, SF', 181, '미국', '12세 이상', '2019-04-24', '안소니 루소, 조 루소', 1400, '로버트 다우니 주니어, 크리스 에반스', 'https://youtube.com/endgame_trailer', '10', 8.8, '/images/endgame.jpg', 30);
+
+INSERT INTO MOVIE (MOVIE_NO, MOVIE_TITLE, MOVIE_CONTENT, GENRE, PLAYTIME, COUNTRY, AGE_LV, OPEN_DATE, DIRECTOR, AUDIENCE_COUNT, ACTOR, PREVIEW, STATUS, GRADE, MAIN_PATH, TASTE_NO) 
+VALUES (SEQ_MOVIENO.NEXTVAL, '인터스텔라', '우주를 탐험하는 인간의 이야기로, 시간과 공간을 넘는 모험을 그린 영화.', 'SF, 드라마', 169, '미국', '12세 이상', '2014-11-06', '크리스토퍼 놀란', 500, '매튜 맥커너히, 앤 해서웨이', 'https://youtube.com/interstellar_trailer', '20', 9.3, '/images/interstellar.jpg', 40);
+
+INSERT INTO MOVIE (MOVIE_NO, MOVIE_TITLE, MOVIE_CONTENT, GENRE, PLAYTIME, COUNTRY, AGE_LV, OPEN_DATE, DIRECTOR, AUDIENCE_COUNT, ACTOR, PREVIEW, STATUS, GRADE, MAIN_PATH, TASTE_NO) 
+VALUES (SEQ_MOVIENO.NEXTVAL, '라라랜드', '재즈 음악과 사랑이 어우러진 꿈을 향한 두 남녀의 이야기.', '뮤지컬, 드라마', 128, '미국', '12세 이상', '2016-12-07', '데이미언 셔젤', 300, '라이언 고슬링, 엠마 스톤', 'https://youtube.com/lalaland_trailer', '20', 8.5, '/images/lalaland.jpg', 50);
+
+commit;
